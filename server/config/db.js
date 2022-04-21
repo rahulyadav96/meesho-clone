9fb92ad1d57084cf94b4e3b7bddd0aaedecaf947
rahulyadav96 <rahul.yadav96@hotmail.com>
2022-04-21 18:28:21 +0530

    restarted warking on project, setup database connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
+
 require('dotenv').config();
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const DB_PASSWORD = process.env.DB_PASSWORD || "sansarapur";
 
 const connect = ()=>{
-    return mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.z8q8a.mongodb.net/meesho?retryWrites=true&w=majority`);
+    return mongoose.connect(`mongodb+srv://mrahul:${DB_PASSWORD}@cluster0.wbced.mongodb.net/meesho?retryWrites=true&w=majority`);
 }
 
 module.exports = connect;
+
+//mongodb+srv://mrahul:${DB_PASSWORD}>@cluster0.wbced.mongodb.net/meesho?retryWrites=true&w=majority
